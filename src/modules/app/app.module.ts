950b7a5e554ac0b180d38e327a217e915dda66b1
admin <admin@admins-MacBook-Pro.local>
2024-11-08 02:08:45 +0700

    base template = todo: handler authorization inside code

---
+++ src/modules/app/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { MiddlewareConsumer } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { PrismaModule } from '../prisma/prisma.module';
+import { GLOBAL_CONFIG } from '../../configs/global.config';
+import { LoggerModule } from '../logger/logger.module';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { LoggerMiddleware } from '../../middlewares/logger.middleware';
+
+@Module({
+  imports: [
+    LoggerModule,
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true, load: [() => GLOBAL_CONFIG] }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+  exports: [],
+})
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
