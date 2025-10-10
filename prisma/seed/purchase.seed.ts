f12d18dd9c009ce37fe13f11f97d844b8495edcc
rucci <d.rucci.2001@gmail.com>
2025-10-10 21:56:56 +0700

    fix

--- prisma/seed/purchase.seed.ts
+++ prisma/seed/purchase.seed.ts
@@ -5,54 +5,50 @@ import { DateHelper } from 'src/shared/helper/date.helper';
 import { UuidHelper } from 'src/shared/helper/uuid.helper';
 
 export async function purchaseSeed(app: INestApplicationContext) {
-  try {
-    const purchaseService = app.get(PurchaseService);
-
-    await purchaseService.create({
-      merchantId: 1,
-      nominal: new Decimal(1000000), // 1 jt
-      providerName: 'NETZME',
-      paymentMethodName: 'QRIS',
-      referenceId: UuidHelper.v4(),
-      code: `${DateHelper.now().toUnixInteger()}-${1}-PURCHASE-NETZME-QRIS`,
-      status: 'PENDING',
-    });
-
-    await purchaseService.create({
-      merchantId: 1,
-      nominal: new Decimal(3500000), // 3.5 jt
-      providerName: 'NETZME',
-      paymentMethodName: 'VIRTUALACCOUNT',
-      referenceId: UuidHelper.v4(),
-      code: `${DateHelper.now().toUnixInteger()}-${1}-PURCHASE-NETZME-VIRTUALACCOUNT`,
-      status: 'EXPIRED',
-    });
-
-    await purchaseService.create({
-      merchantId: 1,
-      nominal: new Decimal(70000000), // 70 jt
-      providerName: 'DANA',
-      paymentMethodName: 'DIRECTEWALLET',
-      referenceId: UuidHelper.v4(),
-      code: `${DateHelper.now().toUnixInteger()}-${1}-PURCHASE-DANA-DIRECTEWALLET`,
-      status: 'SUCCESS',
-    });
-
-    await purchaseService.create({
-      merchantId: 1,
-      nominal: new Decimal(10000000), // 10 jt
-      providerName: 'DANA',
-      paymentMethodName: 'VIRTUALACCOUNT',
-      referenceId: UuidHelper.v4(),
-      code: `${DateHelper.now().toUnixInteger()}-${1}-PURCHASE-DANA-VIRTUALACCOUNT`,
-      status: 'PENDING',
-    });
-  } catch (error) {
-    console.log('Error');
-    console.log(error);
-  } finally {
-    console.log('purchaseSeed Complete');
-  }
+  // try {
+  //   const purchaseService = app.get(PurchaseService);
+  //   await purchaseService.create({
+  //     merchantId: 1,
+  //     nominal: new Decimal(1000000), // 1 jt
+  //     providerName: 'NETZME',
+  //     paymentMethodName: 'QRIS',
+  //     referenceId: UuidHelper.v4(),
+  //     code: `${DateHelper.now().toUnixInteger()}-${1}-PURCHASE-NETZME-QRIS`,
+  //     status: 'PENDING',
+  //   });
+  //   await purchaseService.create({
+  //     merchantId: 1,
+  //     nominal: new Decimal(3500000), // 3.5 jt
+  //     providerName: 'NETZME',
+  //     paymentMethodName: 'VIRTUALACCOUNT',
+  //     referenceId: UuidHelper.v4(),
+  //     code: `${DateHelper.now().toUnixInteger()}-${1}-PURCHASE-NETZME-VIRTUALACCOUNT`,
+  //     status: 'EXPIRED',
+  //   });
+  //   await purchaseService.create({
+  //     merchantId: 1,
+  //     nominal: new Decimal(70000000), // 70 jt
+  //     providerName: 'DANA',
+  //     paymentMethodName: 'DIRECTEWALLET',
+  //     referenceId: UuidHelper.v4(),
+  //     code: `${DateHelper.now().toUnixInteger()}-${1}-PURCHASE-DANA-DIRECTEWALLET`,
+  //     status: 'SUCCESS',
+  //   });
+  //   await purchaseService.create({
+  //     merchantId: 1,
+  //     nominal: new Decimal(10000000), // 10 jt
+  //     providerName: 'DANA',
+  //     paymentMethodName: 'VIRTUALACCOUNT',
+  //     referenceId: UuidHelper.v4(),
+  //     code: `${DateHelper.now().toUnixInteger()}-${1}-PURCHASE-DANA-VIRTUALACCOUNT`,
+  //     status: 'PENDING',
+  //   });
+  // } catch (error) {
+  //   console.log('Error');
+  //   console.log(error);
+  // } finally {
+  //   console.log('purchaseSeed Complete');
+  // }
   // await prisma.$transaction([
   //   prisma.purchaseTransaction.create({
   //     data: {
