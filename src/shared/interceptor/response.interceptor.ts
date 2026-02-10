d37cd8925ef1ea703b63dead52f67196506f8ca6
rucci <d.rucci.2001@gmail.com>
2026-02-10 13:09:57 +0700

    Response Interceptor Skip

--- src/shared/interceptor/response.interceptor.ts
+++ src/shared/interceptor/response.interceptor.ts
@@ -1,16 +1,29 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { map, Observable } from 'rxjs';
 import { Page } from 'src/shared/pagination/pagination';
 import { ResponseDto, ResponseStatus } from 'src/shared/response.dto';
+import { SKIP_RESPONSE_INTERCEPTOR } from './skip-response.interceptor';
 
 export class ResponseInterceptor<T>
-  implements NestInterceptor<T, ResponseDto<T>>
+  implements NestInterceptor<T, ResponseDto<T> | null>
 {
+  constructor(private readonly reflector: Reflector) {}
+
   intercept(
     context: ExecutionContext,
     next: CallHandler<T>,
-  ): Observable<ResponseDto<T>> | Promise<Observable<ResponseDto<T>>> {
+  ):
+    | Observable<ResponseDto<T> | null>
+    | Promise<Observable<ResponseDto<T> | null>> {
     const request = context.switchToHttp().getRequest();
+
+    const skip = this.reflector.getAllAndOverride<boolean>(
+      SKIP_RESPONSE_INTERCEPTOR,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (skip) return next.handle() as Observable<null>;
 
     if (request.path === '/metrics') {
       return next.handle() as Observable<ResponseDto<T>>;
