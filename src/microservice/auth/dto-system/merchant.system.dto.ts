af85fc17250cf4b3df04df8be0821d34b2e0bb4d
rucci <d.rucci.2001@gmail.com>
2026-03-03 15:36:56 +0700

    microservices

--- src/microservice/auth/dto-system/merchant.system.dto.ts
+++ src/microservice/auth/dto-system/merchant.system.dto.ts
@@ -6,10 +6,10 @@ export class MerchantSystemDto {
     DtoHelper.assign(this, data);
   }
   @ApiProperty({ type: Number })
-  merchantId: number;
+  userId: number;
 
   @ApiProperty({ type: Number })
-  userId: number;
+  profileId: number;
 
   @ApiProperty({ type: String })
   email: string;
